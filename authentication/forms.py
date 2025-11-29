6cee854853a19c98adf23e3cb887cb33feba5a42
7aib <zohaibrana03@gmail.com>
2025-11-29 11:30:13 +0500

    added delete modal fixed error msgs

--- authentication/forms.py
+++ authentication/forms.py
@@ -38,5 +38,40 @@ class UserUpdateForm(UserChangeForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.CheckboxInput):
+                field.widget.attrs["class"] = "form-check-input"
+            else:
+                field.widget.attrs["class"] = "form-control"
+
+
+class CustomerRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name", "last_name", "phone_number")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        from .choices import UserRoles
+        user.role = UserRoles.CUSTOMER
+        if commit:
+            user.save()
+        return user
+
+
+class ProfileUpdateForm(UserChangeForm):
+    password = None
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "email", "phone_number")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs["class"] = "form-control"
