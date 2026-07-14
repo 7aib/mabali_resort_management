f079a4b18f9a34f80f7ee77e4257590a793c8b49
7aib <zohaibrana03@gmail.com>
2026-07-15 01:50:06 +0500

    feat: privacy policy

--- mabali_resort_management/urls.py
+++ mabali_resort_management/urls.py
@@ -3,11 +3,12 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import terms_of_service_view
+from .views import terms_of_service_view, privacy_policy_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("terms-of-service/", terms_of_service_view, name='terms_of_service'),
+    path("privacy-policy/", privacy_policy_view, name='privacy_policy'),
     path("", include("authentication.urls")),
     path("", include("dashboard.urls")),
     path("inventory/", include("inventory.urls")),
--- mabali_resort_management/views.py
+++ mabali_resort_management/views.py
@@ -5,3 +5,7 @@ from django.http import HttpRequest, HttpResponse
 
 def terms_of_service_view(request: HttpRequest) -> HttpResponse:
     return render(request, 'pages/terms_of_service.html')
+
+
+def privacy_policy_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'pages/privacy_policy.html')
